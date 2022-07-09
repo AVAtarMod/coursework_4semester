d17e852541fc105cc72d51b8082f39429e3a3a20
AVAtarMod <respawner289@gmail.com>
2022-07-09 21:21:58 +0300

    [code] Fix Task1_ReadNumbersFromUser, Add functions

--- code/functions.cpp
+++ code/functions.cpp
@@ -1,26 +1,52 @@
 #include "functions.hpp"
 
+ComplexNumber intersect(lineSegment_t first, lineSegment_t second)
+{
+    return ComplexNumber::getZero();
+}
+
+bool isPointBelongsSegment(lineSegment_t segment, ComplexNumber point)
+{
+    ComplexNumber &a = segment.first, &b = segment.second;
+
+    double reMax = std::max(a.Re, b.Re), reMin = std::min(a.Re, b.Re);
+    double imMax = std::max(a.Im, b.Im), imMin = std::min(a.Im, b.Im);
+
+    bool belongsByRe = reMin < point.Re && point.Re < reMax;
+    bool belongsByIm = imMin < point.Im && point.Im < imMax;
+
+    return ComplexNumber::isOnSameLine(a, b, point) && belongsByIm && belongsByRe;
+}
+
 void Task1_ReadNumbersFromUser(ComplexNumber arr[5])
 {
     const size_t labelsCount = 5;
     std::string labels[labelsCount] { "a", "b", "c", "a1", "b1" };
+
+    ComplexNumber &a = arr[0],
+                  &b = arr[1],
+                  &c = arr[2],
+                  &a1 = arr[3],
+                  &b1 = arr[4];
 
     bool isTriangle = false, isValidA1 = false, isValidB1 = false;
     while (!(isTriangle && isValidA1 && isValidB1)) {
         std::cout << "Введите координаты точек a,b,c,a1,b1:\n";
         for (size_t i = 0; i < labelsCount; i++) {
             std::cout << "  " << labels[i] << ": ";
-            std::cin >> numbers[i];
+            std::cin >> arr[i];
         }
         isTriangle = a * (b - c) + b * (c - a) + c * (a - b)
             == ComplexNumber::getZero();
-        isValidA1 = ComplexNumber::isOnSameLine(b, c, a1) && (a1.Im < std::max(b.Im, c.Im)) && (a1.Re < std::max(b.Re, c.Re));
-        isValidB1 = ComplexNumber::isOnSameLine(a, c, b1) && (b1.Im < std::max(a.Im, c.Im)) && (b1.Re < std::max(a.Re, c.Re));
+
+        isValidA1 = isPointBelongsSegment({ b, c }, a1);
+        isValidB1 = isPointBelongsSegment({ a, c }, b1);
+
         if (!isTriangle)
-            std::cout << "Некорректные a,b,c. Должны быть точками треугольника ABC\n";
+            std::cerr << "Некорректные a,b,c. Должны быть точками треугольника ABC\n";
         if (!isValidA1)
-            std::cout << "Некорректная a1. Должна быть на отрезке BC\n";
+            std::cerr << "Некорректная a1. Должна быть на отрезке BC\n";
         if (!isValidB1)
-            std::cout << "Некорректная b1. Должна быть на отрезке AC\n";
+            std::cerr << "Некорректная b1. Должна быть на отрезке AC\n";
     }
 }
--- code/functions.hpp
+++ code/functions.hpp
@@ -1,10 +1,20 @@
 #ifndef FUNCTIONS_LIB
 #define FUNCTIONS_LIB
 
-#include "complexn.hpp"
+#include "ComplexNumber.hpp"
+#include "Line.hpp"
+
 #include <tuple>
 
-ComplexNumber intersect(std::pair<ComplexNumber, ComplexNumber> first, std::pair<ComplexNumber, ComplexNumber> second);
+using lineSegment_t = std::pair<ComplexNumber, ComplexNumber>;
+
+/**
+ * @brief Intersect of 2 line segments
+ * @return ComplexNumber, intersection point of lines
+ */
+ComplexNumber intersect(lineSegment_t first, lineSegment_t second);
+
+bool isPointBelongsSegment(lineSegment_t segment, ComplexNumber point);
 
 void Task1_ReadNumbersFromUser(ComplexNumber arr[5]);
 
