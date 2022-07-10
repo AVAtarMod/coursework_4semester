f41277c96792a2295ff1f0175d479006b07e8d3f
AVAtarMod <respawner289@gmail.com>
2022-07-11 00:24:31 +0300

    [code] Line: update

--- code/Line.cpp
+++ code/Line.cpp
@@ -1,10 +1,147 @@
 #include "Line.hpp"
+#include "functions.hpp"
+#include <cmath>
+#include <limits>
+#include <stdexcept>
 
-Line::Line(std::pair<Point, Point> pair){
-    k = 0;
-    b = 0;
+struct EquationDataFromPoints {
+    Point A, B;
+    double eqK, eqB, xConst, yConst;
+    bool inited = false;
+} tmp;
+
+void initTmpFromPoints(const Point& a, const Point& b)
+{
+    if (a == b || std::isinf(a.X()) || std::isinf(a.Y()) || std::isinf(b.X()) || std::isinf(b.Y()))
+        throw std::runtime_error("Cannot create line from 2 equal points, or coordinates incorrect (a.e. Inf)");
+
+    if (!tmp.inited || tmp.A != a || tmp.B != b) {
+        tmp.A = a, tmp.B = b;
+
+        double yDiff = tmp.B.Y() - tmp.A.Y();
+        double xDiff = tmp.B.X() - tmp.A.X();
+
+        int lineType = (xDiff == 0) ? 0 : ((yDiff == 0) ? 1 : 2);
+        switch (lineType) {
+        case 0:
+            // Y may be any, x = const
+            tmp.eqK = 0;
+            tmp.eqB = std::numeric_limits<double>::infinity();
+            tmp.xConst = tmp.B.X();
+            break;
+        case 1:
+            // X may be any, y = const
+            tmp.eqK = 0;
+            tmp.eqB = 0;
+            tmp.yConst = tmp.B.Y();
+            break;
+        case 2:
+            // Normal line, y = kx + b
+            tmp.eqK = yDiff / xDiff;
+            tmp.eqB = (-tmp.A.X() * yDiff + tmp.A.Y() * xDiff) / xDiff;
+            break;
+        default:
+            break;
+        }
+
+        tmp.inited = true;
+    }
+
+    return;
 }
 
-bool Line::isPointBelongsLine(Point point){
-    return y(point.X) == point.Y;
+double Line::getKFromPoints(const Point& a, const Point& b)
+{
+    initTmpFromPoints(a, b);
+    return tmp.eqK;
+}
+double Line::getBFromPoints(const Point& a, const Point& b)
+{
+    initTmpFromPoints(a, b);
+    return tmp.eqB;
+}
+
+Line::Line(std::pair<Point, Point> pair)
+    : _k(getKFromPoints(pair.first, pair.second)),
+      _b(getBFromPoints(pair.first, pair.second))
+{
+    if (std::isinf(_b)) {
+        _type = LineType::CONST_X;
+        _x = tmp.xConst;
+    } else if (_k == 0) {
+        _type = LineType::CONST_Y;
+        _y = tmp.yConst;
+    } else
+        _type = LineType::NORMAL;
+}
+
+double Line::y(double x) const
+{
+    switch (_type) {
+    case LineType::CONST_X:
+        return (_x == x)
+            ? std::numeric_limits<double>::infinity()
+            : 0;
+    case LineType::CONST_Y:
+        return _y;
+    case LineType::NORMAL:
+        return _k * x + _b;
+    default:
+        return 0;
+    }
+}
+
+double Line::x(double y) const
+{
+    switch (_type) {
+    case LineType::CONST_X:
+        return _x;
+    case LineType::CONST_Y:
+        return (_y == y)
+            ? std::numeric_limits<double>::infinity()
+            : 0;
+    case LineType::NORMAL:
+        return (y - _b) / _k;
+    default:
+        return 0;
+    }
+}
+
+bool Line::isInX(double x) const
+{
+    switch (_type) {
+    case LineType::CONST_X:
+        return _x == x;
+    case LineType::CONST_Y:
+    case LineType::NORMAL:
+        return true;
+    default:
+        return false;
+    }
+}
+bool Line::isInY(double y) const
+{
+    switch (_type) {
+    case LineType::CONST_X:
+    case LineType::NORMAL:
+        return true;
+    case LineType::CONST_Y:
+        return _y == y;
+    default:
+        return 0;
+    }
+}
+
+bool Line::isBelongs(Point point)
+{
+    switch (_type) {
+    case LineType::CONST_X:
+        return _x == point.X();
+    case LineType::CONST_Y:
+        return _y == point.Y();
+    case LineType::NORMAL:
+        return almost_equal(y(point.X()), point.Y(), 2);
+    default:
+        return false;
+    }
 }
--- code/Line.hpp
+++ code/Line.hpp
@@ -4,19 +4,47 @@
 #include "Point.hpp"
 #include <tuple>
 
+enum class LineType {
+    CONST_Y, // y = const
+    CONST_X, // x = const
+    NORMAL // y = kx + b
+};
+
 /**
  * @brief Represents line by equation 'y = kx + b'
  */
 class Line {
 private:
-    double k, b;
+    double _k, _b;
+
+    /**
+     * @brief Defines y or x constant value if _type is CONST_X or CONST_Y. This is reference for memory optimization.
+     */
+    double &_x = _b, &_y  = _k;
+    LineType _type;
+
+    static double getKFromPoints(const Point& a, const Point& b);
+    static double getBFromPoints(const Point& a, const Point& b);
 
 public:
-    Line(double k, double b) : k(k), b(b) { }
-    Line(std::pair<Point, Point> pair);
+    
 
-    double y(double x) const { return k * x + b; }
-    bool isPointBelongsLine(Point point);
+    Line(double k, double b) : _k(k), _b(b) { }
+    Line(std::pair<Point, Point> pair);
+    Line(Point first, Point second) : Line(std::make_pair(first, second)) { }
+
+    LineType getType() const { return _type;}
+
+    double y(double x) const;
+    double x(double y) const;
+
+    const double& K() const { return _k; }
+    const double& B() const { return _b; }
+
+    bool isInX(double x) const;
+    bool isInY(double y) const;
+
+    bool isBelongs(Point point);
 };
 
 #endif // LINE_LIB
