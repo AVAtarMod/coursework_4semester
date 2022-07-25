ce37c1772d77271888af307dd0f676fef4093205
AVAtarMod <respawner289@gmail.com>
2022-07-25 18:52:36 +0300

    [code] Update style code; Fix Circle::radius()

--- code/Circle.hpp
+++ code/Circle.hpp
@@ -13,8 +13,8 @@ class Circle
    Circle(const Point& a, const Point& b, const Point& c);
 
    Point center() const { return _center; }
-   unsigned int radius() const { return _radius; }
-   
+   const unsigned int& radius() const { return _radius; }
+
    ~Circle();
 };
 
