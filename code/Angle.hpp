1394a9ade6f751443e009c05f1673e833b548d72
AVAtarMod <respawner289@gmail.com>
2022-07-31 15:36:47 +0300

    [code] Angle: Add operators, fullAngle(), source file

--- code/Angle.hpp
+++ code/Angle.hpp
@@ -4,20 +4,41 @@
 class Angle
 {
   private:
-   double _value;
+   /**
+    * @brief Value in degrees. Must be between 0 and 360
+    */
+   double _degrees;
 
   public:
-  /**
-   * @brief Construct a new Angle object by degrees
-   * 
-   * @param value an angle in degrees
-   */
-   Angle(double value = 0.0) : _value(value) {}
+   /**
+    * @brief Construct a new Angle object by degrees
+    *
+    * @param value an angle in degrees
+    */
+   Angle(double value = 0.0);
+
+   bool operator==(const Angle& other) const;
+   bool operator!=(const Angle& other) const;
+   bool operator>(const Angle& other) const;
+   bool operator<(const Angle& other) const;
+   bool operator<=(const Angle& other) const;
+   bool operator>=(const Angle& other) const;
+
+   Angle operator+(const Angle& other) const;
+   Angle operator-(const Angle& other) const;
+   /**
+    * @brief Divide angle value to number
+    * 
+    * @param value Number that will divides this angle
+    */
+   Angle operator/(const double& number) const;
 
    /**
     * @brief Return value of this angle in degrees
     */
-   double degrees() const { return _value; }
+   double degrees() const { return _degrees; }
+
+   static Angle fullAngle();
 };
 
 #endif // COURSEWORK_4_1_ANGLE _HPP
